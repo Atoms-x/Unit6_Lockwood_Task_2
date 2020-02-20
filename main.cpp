8e184134d9e6ca4dc1099e5677181a0173765ab9
SgtR0ck <chet.lockwood@my.tccd.edu>
2020-02-20 01:10:55 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+	Teacher: Dr. Tyson McMillan, Ph.D. COSC 1436 
+  STUDENT (McMillanite): Chet Lockwood
+  Unit_6_Task_6
+  IDE: Repl.it
+*/
+
+#include <iostream>  // Provides cout, cin, endl
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+  
+  return 0;
+}
